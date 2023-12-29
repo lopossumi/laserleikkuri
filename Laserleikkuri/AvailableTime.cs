4b12f78a6e315e82841cb1dc1023489d13ebec2e
milo <mikko.loponen@iki.fi>
2023-12-29 21:28:07 +0200

    Rehallucinate as a telegram bot

--- Laserleikkuri/AvailableTime.cs
+++
@@ -1,7 +0,0 @@
-public class AvailableTime
-{
-    public DateTime Date { get; set; }
-    public DateTime Start { get; set; }
-    public DateTime End { get; set; }
-}
-
--- Laserleikkuri/Program.cs
+++ Laserleikkuri/Program.cs
@@ -37,162 +37,140 @@
 //
 // The available times should be stored into a json file on disk.
 //
-// When the program is started, it should fetch the data from the API and compare it to the data in the json file.
-// If there are new available times in the API output that do not exist in the file, the program shall output them to the console.
-// Additionally, the program shall always print out all available times to the console.
-//
-// Program shall output the available times to the console as follows:
-//
-// (example 1)
-// New times:
-// *2023-12-01  Free:       10:00 - 11:00 (1h)
-//
-// Calendar:
-//  2023-12-01  Free:       10:00 - 11:00 (1h)
-//  2023-12-04  Free:       11:00 - 14:00 (3h)
-// (end of example 1)
-//
-// (example 2)
-// No new times available.
-//
-// Calendar:
-//  2023-12-01  Free:       10:00 - 11:00 (1h)
-//  2023-12-04  Free:       11:00 - 14:00 (3h)
-// (end of example 2)
-//
-// The file shall be updated with the available times fetched from API.
+// When the program is started, it should periodically fetch the data from the API every 10 minutes and compare it to the data in the json file.
+// If there are new available times in the API output that do not exist in the file, the program shall do the following:
+// * output the new time to the console
+// * update the json file
+// * send a telegram message with a telegram bot to a predefined chat id, containing the new time.
 //
 // Program code starts here:
 
+using System;
+using System.Collections.Generic;
+using System.Configuration;
 using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Text.Json;
+using System.Threading.Tasks;
 using Newtonsoft.Json;
+using JsonSerializer = Newtonsoft.Json.JsonSerializer;
 
-var startDate = DateTime.Now;
-var endDate = startDate.AddDays(14);
 
-var endpoint = $"https://api.hel.fi/respa/v1/resource/axwzr3i57yba/?start={{0}}&end={{1}}&format=json";
-var url = string.Format(
-    endpoint, 
-    startDate.ToString("yyyy-MM-ddTHH:mm:ss", CultureInfo.InvariantCulture), 
-    endDate.ToString("yyyy-MM-ddTHH:mm:ss", CultureInfo.InvariantCulture));
-
-var client = new HttpClient();
-var response = await client.GetAsync(url);
-var content = await response.Content.ReadAsStringAsync();
-var data = JsonConvert.DeserializeObject<RespaData>(content) ?? throw new Exception("Deserialization failed");
-
-var openingHours = data.OpeningHours
-    .Where(x => x.Date >= startDate && x.Date <= endDate)
-    .Where(x => x.Opens != null && x.Closes != null)
-    .ToList();
-
-var reservations = data.Reservations
-    ?.Where(x => x.Begin >= startDate && x.End <= endDate)
-    .ToList();
-
-var availableTimes = new List<AvailableTime>();
-foreach (var openingHour in openingHours)
+namespace Laserleikkuri
 {
-    var reservationsForDay = reservations?.Where(x => x.Begin.Date == openingHour.Date.Date).ToList() 
-        ?? new List<Reservation>();
-
-    if (reservationsForDay.Count == 0)
+    class Program
     {
-        availableTimes.Add(new AvailableTime
-        {
-            Date = openingHour.Date,
-            Start = openingHour.Opens ?? throw new Exception("Opening hour opens is null"),
-            End = openingHour.Closes ?? throw new Exception("Opening hour closes is null")
-        });
-    }
-    else
-    {
-        var reservationsForDayOrdered = reservationsForDay
-            .OrderBy(x => x.Begin)
-            .ToList();
+        static readonly HttpClient client = new HttpClient();
+        static readonly string url = "https://api.hel.fi/respa/v1/resource/axwzr3i57yba/?start={0}&end={1}&format=json";
+        static readonly string jsonFile = "data.json";
+        static readonly TimeSpan interval = TimeSpan.FromMinutes(10);
+        private static string telegramBotToken = "";
+        private static string telegramChatId = "";
 
-        var lastReservation = reservationsForDayOrdered[0];
-        if (lastReservation.Begin > openingHour.Opens)
+        static async Task Main(string[] args)
         {
-            availableTimes.Add(new AvailableTime
+
+            // read telegram bot token and chat id from file telegram_config.json
+            var telegramConfig = JsonConvert.DeserializeObject<Dictionary<string, string>>(File.ReadAllText("telegram_config.json")) 
+                ?? throw new ConfigurationErrorsException("Invalid telegram bot configuration.");
+            telegramBotToken = telegramConfig["bot_token"];
+            telegramChatId = telegramConfig["chat_id"];
+
+            while (true)
             {
-                Date = openingHour.Date,
-                Start = openingHour.Opens ?? throw new Exception("Opening hour opens is null"),
-                End = lastReservation.Begin
-            });
-        }
-
-        for (var i = 0; i < reservationsForDayOrdered.Count - 1; i++)
-        {
-            var currentReservation = reservationsForDayOrdered[i];
-            var nextReservation = reservationsForDayOrdered[i + 1];
-
-            if (nextReservation.Begin > currentReservation.End)
-            {
-                availableTimes.Add(new AvailableTime
+                try
                 {
-                    Date = openingHour.Date,
-                    Start = currentReservation.End,
-                    End = nextReservation.Begin
-                });
+                    await CheckAvailability();
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+                await Task.Delay(interval);
             }
         }
 
-        var firstReservation = reservationsForDayOrdered[^1];
-        if (firstReservation.End < openingHour.Closes)
+        static async Task CheckAvailability()
         {
-            availableTimes.Add(new AvailableTime
+            var startDate = DateTime.Now;
+            var endDate = startDate.AddDays(30);
+            var response = await client.GetAsync(
+                string.Format(
+                    url,
+                    startDate.ToString("yyyy-MM-ddTHH:mm", CultureInfo.InvariantCulture),
+                    endDate.ToString("yyyy-MM-ddTHH:mm", CultureInfo.InvariantCulture)));
+            response.EnsureSuccessStatusCode();
+            var responseBody = await response.Content.ReadAsStringAsync();
+            var data = JsonConvert.DeserializeObject<Root>(responseBody);
+            var availableTimes = new List<Reservation>();
+            foreach (var openingHours in data.OpeningHours)
             {
-                Date = openingHour.Date,
-                Start = firstReservation.End,
-                End = openingHour.Closes ?? throw new Exception("Opening hour closes is null")
-            });
+                if (openingHours.Opens == null || openingHours.Closes == null)
+                {
+                    continue;
+                }
+                var opens = DateTime.Parse(openingHours.Opens);
+                var closes = DateTime.Parse(openingHours.Closes);
+                var reservations = data.Reservations.Where(r => r.Begin >= opens && r.End <= closes);
+                var available = new List<Reservation>();
+                var begin = opens;
+                foreach (var reservation in reservations)
+                {
+                    if (reservation.Begin > begin)
+                    {
+                        available.Add(new Reservation { Begin = begin, End = reservation.Begin });
+                    }
+                    begin = reservation.End;
+                }
+                if (begin < closes)
+                {
+                    available.Add(new Reservation { Begin = begin, End = closes });
+                }
+                availableTimes.AddRange(available);
+            }
+            var json = JsonConvert.SerializeObject(availableTimes);
+            if (File.Exists(jsonFile))
+            {
+                var oldJson = File.ReadAllText(jsonFile);
+                if (json == oldJson)
+                {
+                    return;
+                }
+            }
+            File.WriteAllText(jsonFile, json);
+
+            var output = availableTimes.Select(x => $"{x.Begin.ToShortDateString()}: {x.Begin.ToShortTimeString()} - {x.End.ToShortTimeString()}");
+            Console.WriteLine(string.Join(Environment.NewLine, output));
+            await SendTelegramMessage($"Next available times:{Environment.NewLine}{string.Join(Environment.NewLine, output)}");
+        }
+
+        static async Task SendTelegramMessage(string message)
+        {
+            var response = await client.GetAsync($"https://api.telegram.org/bot{telegramBotToken}/sendMessage?chat_id={telegramChatId}&text={message}");
+            response.EnsureSuccessStatusCode();
         }
     }
 }
 
-var availableTimesOrdered = availableTimes
-    .OrderBy(x => x.Date)
-    .ThenBy(x => x.Start)
-    .ToList();
-
-List<AvailableTime> availableTimesOld;
-if (!File.Exists("availableTimes.json"))
+public class Root
 {
-    var availableTimesJsonString = JsonConvert.SerializeObject(availableTimesOrdered, Formatting.Indented);
-    await File.WriteAllTextAsync("availableTimes.json", availableTimesJsonString);
-    availableTimesOld = new List<AvailableTime>();
-}
-else
-{
-    var availableTimesJsonOld = await File.ReadAllTextAsync("availableTimes.json");
-    availableTimesOld = JsonConvert.DeserializeObject<List<AvailableTime>>(availableTimesJsonOld) ?? throw new Exception("Deserialization failed");
+    [JsonProperty("opening_hours")]
+    public List<OpeningHours> OpeningHours { get; set; }
+    [JsonProperty("reservations")]
+    public List<Reservation> Reservations { get; set; }
 }
 
-var availableTimesJson = JsonConvert.SerializeObject(availableTimesOrdered, Formatting.Indented);
-await File.WriteAllTextAsync("availableTimes.json", availableTimesJson);
-
-var newAvailableTimes = availableTimesOrdered
-    .Where(x => !availableTimesOld.Any(y => y.Date == x.Date && y.Start == x.Start && y.End == x.End))
-    .ToList();
-
-if (newAvailableTimes.Count > 0)
+public class OpeningHours
 {
-    Console.WriteLine("New times:");
-    foreach (var newAvailableTime in newAvailableTimes)
-    {
-        Console.WriteLine($"*{newAvailableTime.Date:yyyy-MM-dd}  Free:       {newAvailableTime.Start:HH:mm} - {newAvailableTime.End:HH:mm} ({(newAvailableTime.End - newAvailableTime.Start).TotalHours}h)");
-    }
-}
-else
-{
-    Console.WriteLine("No new times available.");
+    public DateTime Date { get; set; }
+    public string Opens { get; set; }
+    public string Closes { get; set; }
 }
 
-Console.WriteLine();
-Console.WriteLine("Calendar:");
-
-foreach (var availableTime in availableTimesOrdered)
+public class Reservation
 {
-    Console.WriteLine($" {availableTime.Date:yyyy-MM-dd}  Free:       {availableTime.Start:HH:mm} - {availableTime.End:HH:mm} ({(availableTime.End - availableTime.Start).TotalHours}h)");
+    public DateTime Begin { get; set; }
+    public DateTime End { get; set; }
 }
