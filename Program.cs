71018ce72efee3d2a4fed11089bb724249761fdc
milo <mikko.loponen@iki.fi>
2023-12-29 19:48:15 +0200

    Compare fetched available times to previous run

--- Program.cs
+++ Program.cs
@@ -1,7 +1,9 @@
-﻿using Newtonsoft.Json;
-
-// Fetch data from API from address https://api.hel.fi/respa/v1/resource/axwzr3i57yba/?end={endDate}&format=json&start={startDate}
+﻿// # Laserleikkuri
+//
+// Instructions for the task:
+// Fetch data from API from address https://api.hel.fi/respa/v1/resource/axwzr3i57yba/?start={startDate}&end={endDate}&format=json
 // and find the available times for the next month.
+//
 // The input looks something like this:
 // {
 //     "opening_hours": [
@@ -32,9 +34,37 @@
 //         }
 //     ]
 // }
-// The output should look something like this, with the available times for the next month:
-// 2021-09-01   Free:       10:00 - 12:00 (2h)
-// 2021-09-04   Free:       11:00 - 14:00 (3h)
+//
+// The available times should be stored into a json file on disk.
+//
+// When the program is started, it should fetch the data from the API and compare it to the data in the json file.
+// If there are new available times in the API output that do not exist in the file, the program shall output them to the console.
+// Additionally, the program shall always print out all available times to the console.
+//
+// Program shall output the available times to the console as follows:
+//
+// (example 1)
+// New times:
+// *2023-12-01  Free:       10:00 - 11:00 (1h)
+//
+// Calendar:
+//  2023-12-01  Free:       10:00 - 11:00 (1h)
+//  2023-12-04  Free:       11:00 - 14:00 (3h)
+// (end of example 1)
+//
+// (example 2)
+// No new times available.
+//
+// Calendar:
+//  2023-12-01  Free:       10:00 - 11:00 (1h)
+//  2023-12-04  Free:       11:00 - 14:00 (3h)
+// (end of example 2)
+//
+// The file shall be updated with the available times fetched from API.
+//
+// Program code starts here:
+
+using Newtonsoft.Json;
 
 var startDate = DateTime.Now;
 var endDate = startDate.AddMonths(1);
@@ -115,7 +145,48 @@ foreach (var openingHour in openingHours)
     }
 }
 
-foreach (var availableTime in availableTimes)
+var availableTimesOrdered = availableTimes
+    .OrderBy(x => x.Date)
+    .ThenBy(x => x.Start)
+    .ToList();
+
+List<AvailableTime> availableTimesOld;
+if (!File.Exists("availableTimes.json"))
 {
-    Console.WriteLine($"{availableTime.Date:yyyy-MM-dd}   Free:       {availableTime.Start:HH:mm} - {availableTime.End:HH:mm} ({(availableTime.End - availableTime.Start).TotalHours}h)");
+    var availableTimesJsonString = JsonConvert.SerializeObject(availableTimesOrdered, Formatting.Indented);
+    await File.WriteAllTextAsync("availableTimes.json", availableTimesJsonString);
+    availableTimesOld = new List<AvailableTime>();
+}
+else
+{
+    var availableTimesJsonOld = await File.ReadAllTextAsync("availableTimes.json");
+    availableTimesOld = JsonConvert.DeserializeObject<List<AvailableTime>>(availableTimesJsonOld) ?? throw new Exception("Deserialization failed");
+}
+
+var availableTimesJson = JsonConvert.SerializeObject(availableTimesOrdered, Formatting.Indented);
+await File.WriteAllTextAsync("availableTimes.json", availableTimesJson);
+
+var newAvailableTimes = availableTimesOrdered
+    .Where(x => !availableTimesOld.Any(y => y.Date == x.Date && y.Start == x.Start && y.End == x.End))
+    .ToList();
+
+if (newAvailableTimes.Count > 0)
+{
+    Console.WriteLine("New times:");
+    foreach (var newAvailableTime in newAvailableTimes)
+    {
+        Console.WriteLine($"*{newAvailableTime.Date:yyyy-MM-dd}  Free:       {newAvailableTime.Start:HH:mm} - {newAvailableTime.End:HH:mm} ({(newAvailableTime.End - newAvailableTime.Start).TotalHours}h)");
+    }
+}
+else
+{
+    Console.WriteLine("No new times available.");
+}
+
+Console.WriteLine();
+Console.WriteLine("Calendar:");
+
+foreach (var availableTime in availableTimesOrdered)
+{
+    Console.WriteLine($" {availableTime.Date:yyyy-MM-dd}  Free:       {availableTime.Start:HH:mm} - {availableTime.End:HH:mm} ({(availableTime.End - availableTime.Start).TotalHours}h)");
 }
